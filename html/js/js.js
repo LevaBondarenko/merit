f9d6381e082ebacb08d19e4027629de5bccb6a2a
Лев <lion.bond93@gmail.com>
2016-06-30 08:38:03 +0600

    hello

---
+++ html/js/js.js
@@ -0,0 +1,4 @@
+jQuery(document).ready(function($){
+	$('.services__nav li:first-child').prepend('<i class="fa fa-cogs" aria-hidden="true"> </i>');
+	$('.info_list li').prepend('<i class="fa fa-check" aria-hidden="true"></i>');
+})
